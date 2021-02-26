c786db4986e86ec44e0f0a1dafdad480c3065f08
ManiahMellow <jellakus@gmail.com>
2021-02-26 19:28:51 +0700

    REACT-JS #2
    Tugas REACT-JS video 11 - 20

---
+++ REACT-JS #2/project-react-resto/react-resto/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import logo from './logo.svg';
+import Front from './Front/Front';
+import Back from './Back/Back';
+import {BrowserRouter as Router, Route} from "react-router-dom"
+
+function App() {
+  return (
+    <div className="container">
+      <Router>
+        <Route path='/' component={Front} exact/>
+        <Route path='/admin' component={Back}/>
+        <Route path='/home' component={Front}/>
+        
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT-JS #2/project-react-resto/react-resto/src/Front/Front.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Front = () => {
+    return (
+        <>
+            <div className="row">
+                <div className="">Top</div>
+            </div>
+            <div className="row">
+                <div className="col-4">Side</div>
+                <div className="col-8">Main</div>
+            </div>
+            <div className="row">
+                <div className="">Footer</div>
+            </div>
+        </>
+    );
+}
+
+export default Front;
